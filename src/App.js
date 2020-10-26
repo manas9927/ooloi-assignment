9d834263fd8fe0bde211373119feacdd159da775
manas9927 <manasvjbp27@gmail.com>
2020-10-26 14:52:50 +0530

    added: router and material ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from "react"
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+import {
+    Route, BrowserRouter as Router, Switch, Link
+} from "react-router-dom"
+
+import Input from "./pages/input/"
+import Display from "./pages/display/"
 
 function App() {
     return(
-        <div>
-            <h1>Hello</h1>
-        </div>
+        <Router>
+            <div>
+                <Link to="input">
+                    <Button variant="outlined" color="primary">Input</Button>
+                </Link>
+                <Link to="display">
+                    <Button variant="outlined" color="primary">Display</Button>
+                </Link>
+            </div>
+            <Switch>
+                {/* <Route path="/Hello" component={Hello} /> */}
+                <Route path="/display" component={Display} />
+                <Route path="/input" exact component={Input} />
+                
+            </Switch>
+        </Router>
     )
 }
 
---
+++ src/pages/display/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default() => (
+        <div>
+            <h1>Display</h1>
+        </div>
+    )
